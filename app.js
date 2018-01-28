c13e9754dac26885f5ba16d9655cd28d6caf2328
tomaz <TomazWang.tw@gmail.com>
2018-01-28 23:02:23 +0800

    separate router.  remove json parser.

--- app.js
+++ app.js
@@ -20,13 +20,14 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/', lineBot);
+app.use('/bots/line/callback', lineBot);
 
 
 // catch 404 and forward to error handler
@@ -39,18 +40,21 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
 
-    logger.error('error', `handler: err = ${JSON.stringify(err)}`);
+    logger.error('An error occurs:');
+    logger.error(`req.body = ${JSON.stringify(req.body)}`);
+    logger.error(`${err.stack}`);
 
-    console.log(`app.js::39/err handler: err = ${JSON.stringify(err)}`);
-    console.log(`app.js::39/err handler: err.message = \'${err.message}\'`);
+    const status = err.status || 500;
+    logger.error(`res status = ${status}`);
 
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
+    res.status(status);
     res.render('error');
 });
+
 
 module.exports = app;
--- routes/line-bot.js
+++ routes/line-bot.js
@@ -18,30 +18,39 @@ const config = {
 // create LINE SDK client
 const client = new line.Client(config);
 
-
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-router.post('/callback', line.middleware(config), (req, res) => {
+router.post('/',
+    // logging post
+    (req, res, next) => {
+        logger.debug('a post recieved');
+        logger.debug(`req.body = ${JSON.stringify(req.body)}`);
+        next();
+    },
+    // line middleware
+    line.middleware(config),
+    // handle response
+    (req, res) => {
+        const events = req.body.events;
+        logger.debug(`line-bot.js::24/post: event = ${JSON.stringify(events)}`);
 
-    logger.debug('get a post');
-    logger.debug(`line-bot.js::24/post: event = ${JSON.stringify(event)}`);
-
-    console.log('get a post');
-    console.log(`line-bot.js::24/post: event = ${JSON.stringify(event)}`);
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-});
+        // console.log('get a post');
+        // console.log(`line-bot.js::24/post: event = ${JSON.stringify(event)}`);
+        Promise
+            .all(events.map(handleEvent))
+            .then((result) => res.json(result));
+    }
+);
 
 // event handler
 function handleEvent(event) {
     logger.debug(`line-bot.js::32/handleEvent: event.type = ${event.type}`);
-    console.log(`line-bot.js::32/handleEvent: event.type = ${event.type}`);
+    // console.log(`line-bot.js::32/handleEvent: event.type = ${event.type}`);
 
     if (event.type !== 'message' || event.message.type !== 'text') {
         // ignore non-text-message event for now.
         logger.debug('ignored!  not message.');
-        console.log('ignored!  not message.');
+        // console.log('ignored!  not message.');
         return Promise.resolve(null);
     }
 
