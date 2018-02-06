b1bb0d7e82352086bc68260ae3c7b18893dbf05c
Tomaz Wang <TomazWang.tw@gmail.com>
2018-02-07 00:55:44 +0800

    [tomaz-bot-2] fix user json
    Took 25 minutes

--- test/testuserdao.js
+++ test/testuserdao.js
@@ -3,15 +3,16 @@
 const {expect} = require('chai');
 const userDao = require('../lib/db/user/userdao');
 const User = require('../lib/db/user/user');
+const LineProfile = require('../lib/db/user/lineprofile');
 
 
 describe('addUser()', () => {
-  it('should convert a ChatMessage to a LineMessage', () => {
+  it('should add a user to firestore', () => {
 
     // ARRANGE
     let user = new User("John");
     user.name = "Waka";
-    user.lineId = "ssss line";
+    user.lineProfile = new LineProfile("sss line");
 
     // ACT
     userDao.addUser(user);
@@ -25,18 +26,21 @@ describe('addUser()', () => {
 
 
 describe('getUserByLineId()', () => {
-  it('should convert a ChatMessage to a LineMessage', () => {
+  it('should find me a user', () => {
 
     userDao.getUserByLineId("ssss line").then(user => {
       expect(user.lineId).to.be('ssss line');
       expect(user.name).to.be('waka');
     });
+  });
 
-
-  })
-
-
+  it('should return a false', () => {
+    userDao.getUserByLineId("ssss").then(user => {
+      expect(user).to.be(false);
+    });
+  });
 });
+
 
 
 
