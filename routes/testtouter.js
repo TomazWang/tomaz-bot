923cb1d41de1e2507481ba7cb994dac0d097a648
Tomaz Wang <TomazWang.tw@gmail.com>
2018-02-07 00:21:22 +0800

    [tomaz-bot-2] add log to make sure private key is on
    Took 9 minutes

--- routes/testtouter.js
+++ routes/testtouter.js
@@ -7,16 +7,6 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  db.collection('users').get()
-    .then(snap => {
-      snap.forEach((doc) => {
-        res.send(`${doc.id} -> ${JSON.stringify(doc.data())}`);
-      })
-    }).catch(err => {
-    console.log(err);
-  })
-
-
 });
 
 module.exports = router;
