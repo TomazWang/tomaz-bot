7f046007d69556197645f212da6b15a5130b8ef5
tomaz <TomazWang.tw@gmail.com>
2018-02-10 21:01:42 +0800

    fix firestore test
    Took 10 minutes

--- test/testsubcollection.js
+++ test/testsubcollection.js
@@ -1,55 +1,46 @@
 // testsubcollection.js
-
-const userDao = require('../lib/db/user/userDao');
-const User = require('../lib/db/user/User');
-const LineProfile = require('../lib/db/user/LineProfile');
+require('dotenv').config();
 
 const chai = require('chai');
-const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
+
 chai.use(chaiAsPromised);
 
-const admin = require("firebase-admin");
+const config = require('../lib/db/firebaeConfig');
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(config),
+    databaseURL: 'https://tomaz-bot.firebaseio.com',
+  });
+}
+
+
 const db = admin.firestore();
 
-
 describe('basic firestore ussage', () => {
-
   it('should add a data in collection \'sandbox\'', () => {
-    let basicTestRef = db.collection('sandbox').doc('basic_test');
+    const basicTestRef = db.collection('sandbox').doc('basic_test');
     basicTestRef.set({});
   });
-
 });
 
 describe('add sub collection', () => {
-
   it('should add a collection under a doc', () => {
-    let someRef = db.collection('sandbox').doc('sub_collection_root').collection('sub_collection').doc('somthing');
+    const someRef = db.collection('sandbox').doc('sub_collection_root').collection('sub_collection').doc('somthing');
     someRef.set({
-      name: 'hi'
+      name: 'hi',
     });
-  });
-
-
-  it('should get a sub collection', () => {
-    let someRef = db.collection('sandbox/sub_collection_root/sub_collection').doc('somthing');
-    let result = someRef.get().then(snap => {
-      return snap.data()
-    });
-    return expect(result).to.eventually.have.property('name').that.is.equal('hi');
   });
 
   it('should work', () => {
-    if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-    let plateform = process.env.FIREBASE_PLATEFORM;
+    const plateform = process.env.FIREBASE_PLATEFORM;
 
     function getRootRef() {
       return db.collection('plateforms').doc(plateform);
     }
 
     getRootRef().collection('sandbox').doc('sb1').set({});
-  })
-
-
+  });
 });
--- test/textLineBot.js
+++ test/textLineBot.js
@@ -1,29 +1,23 @@
 // testLineBot.js
-const {expect} = require('chai');
-const ChatMessage = require('../lib/chatbot/ChatMessage');
+const { expect } = require('chai');
 const ResponseMessage = require('../lib/chatbot/ResponseMessage');
 const LineBot = require('../lib/line/LineBot');
-const lineBot = new LineBot();
 
-
-describe('test convertToLineMessage()', function () {
-  it('should convert a response message to a line message', function () {
+describe('test convertToLineMessage()', () => {
+  it('should convert a response message to a line message', () => {
     // ARRANGE
-    const desStr = "";
-    const typeStr = "text";
-    const contextStr = "123456";
+    const desStr = '';
+    const typeStr = 'text';
+    const contextStr = '123456';
 
     const resMsg = new ResponseMessage(desStr, typeStr, contextStr);
 
     // ACT
-    const lineMessge = lineBot.convertToLineMessage(resMsg);
+    const lineMessge = LineBot.convertToLineMessage(resMsg);
 
 
     // 3. ASSERT
     expect(lineMessge.type).to.be.equal(typeStr);
     expect(lineMessge.text).to.be.equal(contextStr);
-
   });
-
-
 });
